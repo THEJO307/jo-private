4c3094424c05ba6e7722ac536061937de629a790
ramzkelazzz <116707568+ramzkelazzz@users.noreply.github.com>
2022-10-28 10:41:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'THE ALIF BOT'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['15055017240','15055017240','15055017240']
+global.premium = ['15055017240']
+global.packname = 'The Alif Bot'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
